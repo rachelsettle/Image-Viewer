01ca53f166aea8d847d05237cf63f8db27bcc449
Rachel Settle <rs332@duke.edu>
2017-01-19 20:14:03 -0500

    Image Viewer Commit 1

---
+++ ImageViewer/app/src/main/java/compsci290/edu/duke/imageviewer/ImageGridActivity.java
@@ -0,0 +1,24 @@
+package compsci290.edu.duke.imageviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+/**
+ * Created by Settle on 1/18/17.
+ */
+
+public class ImageGridActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_imagegrid);
+        // Get GridView from xml
+        GridView gridView = (GridView) findViewById (R.id.grid_view);
+
+        // Set Adapter for GridView
+        gridView.setAdapter (new ImageAdapter (this));
+
+    }
+}
